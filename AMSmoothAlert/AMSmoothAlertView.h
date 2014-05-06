aeda9e586144219a5b1c1427ff88c81f9b1de5a8
Antoine <mtonio91@gmail.com>
2014-05-06 14:42:25 -0400

    Podspec added + folder updated

--- AMSmoothAlert/AMSmoothAlertView.h
+++ AMSmoothAlert/AMSmoothAlertView.h
@@ -17,6 +17,8 @@
 @property (nonatomic, assign) float cornerRadius;
 @property (nonatomic, assign) bool isDisplayed;
 @property (nonatomic, assign) AnimationType animationType;
+@property (nonatomic, strong) UILabel * titleLabel;
+@property (nonatomic, strong) UILabel * textLabel;
 @property (nonatomic, strong) UIButton *defaultButton;
 @property (nonatomic, strong) UIButton *cancelButton;
 @property (nonatomic, strong) UIImageView *logoView;
@@ -24,9 +26,11 @@
 @property (nonatomic, strong) UIFont *textFont;
 
 
-- (id) initDropAlertWithTitle:(NSString*) title andText:(NSString*) text forAlertType:(AlertType) type;
-- (id) initFadeAlertWithTitle:(NSString*) title andText:(NSString*) text forAlertType:(AlertType) type;
+- (id) initDropAlertWithTitle:(NSString*) title andText:(NSString*) text andCancelButton:(BOOL)hasCancelButton forAlertType:(AlertType) type;
+- (id) initFadeAlertWithTitle:(NSString*) title andText:(NSString*) text andCancelButton:(BOOL)hasCancelButton forAlertType:(AlertType) type;
 - (void) setCornerRadius:(float)cornerRadius;
+-(void) setTitleText:(NSString*) string;
+-(void) setMessageText:(NSString*) string;
 - (void) show;
 - (void) dismissAlertView;
 
